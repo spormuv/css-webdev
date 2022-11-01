d8c4d9081cca0ed1e08839b2dc36bec55b5675b2
Mike <spormuv@gmail.com>
2022-11-01 19:36:49 +0300

    feat: add slider

---
+++ pulse-gulp-bem/src/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 800,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
