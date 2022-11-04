b21948875cd3e22c05d92fcec9f0e4db4af4771f
Mike <spormuv@gmail.com>
2022-11-04 23:10:31 +0300

    feat: add tabs

--- pulse-gulp-bem/src/js/script.js
+++ pulse-gulp-bem/src/js/script.js
@@ -18,4 +18,46 @@ $(document).ready(function () {
       },
     ],
   });
+  $('ul.catalog__tabs').on(
+    'click',
+    'li:not(.catalog__tab_active)',
+    function () {
+      $(this)
+        .addClass('catalog__tab_active')
+        .siblings()
+        .removeClass('catalog__tab_active')
+        .closest('div.container')
+        .find('div.catalog__content')
+        .removeClass('catalog__content_active')
+        .eq($(this).index())
+        .addClass('catalog__content_active');
+    }
+  );
 });
+
+//tab cards toggle class
+
+const more = document.querySelectorAll('.catalog-item__link');
+const back = document.querySelectorAll('.catalog-item__back');
+
+function toggleClass() {
+  const wrapper = this.closest('div.catalog-item__wrapper');
+  wrapper
+    .querySelector('.catalog-item__content')
+    .classList.toggle('catalog-item__content_active');
+  wrapper
+    .querySelector('.catalog-item__list')
+    .classList.toggle('catalog-item__list_active');
+}
+
+function addEvent(btn) {
+  btn.forEach((item) =>
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      toggleClass.call(this);
+    })
+  );
+}
+
+addEvent(more);
+addEvent(back);
