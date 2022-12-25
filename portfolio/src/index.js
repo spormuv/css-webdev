81703f77c63142429569afa0ee632807c449d3c6
Mike <spormuv@gmail.com>
2022-12-25 21:00:02 +0300

    feat: add media rules

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import './index.html';
-// import './policy.html';
 import './style.scss';
 
 // hamburger
@@ -27,4 +26,14 @@ const counters = document.querySelectorAll('.skills__ratings-counter');
 counters.forEach((counter) => {
   const span = counter.parentElement.querySelector('span');
   span.style.width = counter.textContent;
+});
+
+// promo buttons
+const promoBtns = document.querySelector('.promo__btns');
+promoBtns.addEventListener('click', function (e) {
+  const links = promoBtns.querySelectorAll('.promo__link');
+  if (e.target.classList.contains('promo__link')) {
+    links.forEach((link) => link.classList.remove('btn'));
+    e.target.classList.add('btn');
+  }
 });
